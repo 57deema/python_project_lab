6893370add058d24ab637e86aa53b91f4bb9e125
Deema Wahdan <60104071@udst.edu.qa>
2025-02-02 12:55:42 +0300

    commiting

--- .ipynb_checkpoints/main-checkpoint.py
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -6,4 +6,4 @@ data = load_data()
 X_train, X_test, y_train, y_test = split_data(data)
 model = train_model(X_train, y_train)
 report = evaluate_model(model, X_test, y_test)
-print(report
+print(report)
